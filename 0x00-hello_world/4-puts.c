447685c31034fe7bc8343ca2dfa9fef119bdd930
Tomisin Ajiboye <tomieajiboye@gmail.com>
2022-10-13 04:59:28 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,28 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
-
-
-
 /**
+ * main - Print in C
  *
- * * main - Print C
- *
- * *
- *
- * * Return: Always 0 (Successful)
- *
- * */
-
+ * Return: Always 0 (Successful)
+ */
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual
-
-			puzzle");
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
-
-
