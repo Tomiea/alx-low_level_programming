1cd250fdfc2c94449bbe18c705b202f732a485c4
TOMISIN AJIBOYE <tomieajiboye@gmail.com>
2022-10-18 14:10:28 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include <unistd.h>
 #include "main.h"
 
 /**
- * main - Check description
- * Description: It prints the word _putchar followed by a new line
+ * main - prints _putchar and a new line
  * Return: Always 0
  */
 int main(void)
 {
-
-	char word[8] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-		_putchar(word[i]);
-	_putchar('\n');
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
 
 	return (0);
 }
