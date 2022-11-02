ab344021027e683d8e22be21a3e7de1ea34e05fe
TOMISIN AJIBOYE <tomieajiboye@gmail.com>
2022-11-02 04:37:07 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _puts_recursion - Prints a string, followed by a new line.   
- * @s: The string to be printed.
+ * _puts_recursion - prints a string followed by a newline
+ * @s: the string we print
  * Return: void
  */
 void _puts_recursion(char *s)
